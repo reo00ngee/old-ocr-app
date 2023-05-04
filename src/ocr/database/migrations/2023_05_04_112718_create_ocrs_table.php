a2f36f76dc4badc26808609c605a53cca4a96a1e
reo00ngee <reo00ngee@gmail.com>
2023-05-04 20:53:28 +0900

    a

---
+++ src/ocr/database/migrations/2023_05_04_112718_create_ocrs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ocrs', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('status');
+            $table->string('image');
+            $table->text('content');
+            $table->text('fixed_content');
+            $table->text('point_of_content');
+            $table->timestamp('added_at', $precision = 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ocrs');
+    }
+};
